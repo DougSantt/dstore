e2c28e7fe7a367a00b1a89cd3d02e171076f9bf5
DougSantt <ds076345@gmail.com>
2025-09-12 21:14:14 -0300

    criando validacoes nos inputs

---
+++ formulario/javascript/form.js
@@ -0,0 +1,17 @@
+const form = document.querySelector('#form')
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+
+    const name = document.querySelector('#name').value
+    console.log(name)
+})
+
+const passwordIcons = document.querySelectorAll(".password-icon")
+passwordIcons.forEach(icon =>{
+    icon.addEventListener('click',function(){
+        const input = this.parentElement.querySelector('.form-control')
+        input.type = input.type === 'password' ? 'text' : 'password'
+        this.classList.toggle('bi-eye')
+    })
+})
