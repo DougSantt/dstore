19504f30785566a329e12e1d413142c0ca009ade
DougSantt <ds076345@gmail.com>
2025-12-19 17:54:59 -0300

    desenvolvendo o carrinho

--- products/product-js/product.js
+++ products/product-js/product.js
@@ -1,4 +1,6 @@
-const cartBoxIcon = document.querySelector('.cartBoxIcon')
-cartBoxIcon.addEventListener('click',()=>{
-    cartBoxIcon.classList.toggle('cart')
-})
+// const cartBoxIcon = document.querySelector('.cartBoxIcon')
+// const cartIcon = document.querySelector('cartIcon')
+
+// cartBoxIcon.addEventListener('click',()=>{
+//     cartBoxIcon.classList.toggle('cart')
+// })
